9dc6819bf10eb73a6db1c33e399b7a10f609d220
dirarmdn <dhiraramadini@gmail.com>
2024-06-01 06:14:45 +0700

    feat: added cutscene v0.0

---
+++ Assets/Script/mainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadSceneAsync(2);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
