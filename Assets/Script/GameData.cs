6c6262792a8a53df74ac11c5c500130d5bbbdcb5
iamPanda4 <abulkhoirfaris0@gmail.com>
2024-06-28 18:28:26 +0700

    save load durability alat

--- Assets/Script/GameData.cs
+++ Assets/Script/GameData.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 [System.Serializable]
 public class GameData
 {
-    public int day, money, expenses, grossProfit, orderCompleted;
+    public int day, money, expenses, grossProfit, orderCompleted, galon;
     public float time;
     public int spawnDelay;
     public int jumlahJamuSederhana, jumlahJamuKunyitAsam, jumlahJamuBerasKencur, jumlahJamuPahitan, jumlahJamuTemulawak;
     public GamePersistenceManager.DifficultyLevel difficulty;
-    public SerializableDictionary<string, int> slotItems;
+    public SerializableDictionary<string, int> slotItems, alat;
     public List<int> unlockedJamuIndexes; // Menyimpan indeks jamu yang telah dibuka
 
     public GameData()
@@ -27,6 +27,8 @@ public class GameData
         this.jumlahJamuTemulawak = 0;
         this.difficulty = GamePersistenceManager.DifficultyLevel.Easy; // Default difficulty
         this.slotItems = new SerializableDictionary<string, int>();
+        this.alat = new SerializableDictionary<string, int>();
+        this.galon = 55;
         this.unlockedJamuIndexes = new List<int>(); // Defaultnya kosong
     }
 }
--- Assets/Script/alatDapur.cs
+++ Assets/Script/alatDapur.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 
-public class alatDapur : MonoBehaviour
+public class alatDapur : MonoBehaviour, IDataPersistence
 {
-    public int durability = 100;
+    public int durability;
+    public string name;
     public int currentDurability;
     public SpriteRenderer spriteRenderer;
     public Sprite brokenSprite;
     public Sprite normalSprite;
     void Start()
     {
-        currentDurability = durability;
+        
     }
     public void kurang()
     {
@@ -26,5 +27,28 @@ public class alatDapur : MonoBehaviour
     {
         currentDurability = durability;
         spriteRenderer.sprite = normalSprite;
+    }
+
+    public void LoadData(GameData data)
+    {
+        if (data.alat.TryGetValue(name, out int savedDurability))
+        {
+            Debug.Log("Load " + name + " " + savedDurability);
+            currentDurability = savedDurability;
+            if(currentDurability <= 0)
+            {
+                spriteRenderer.sprite = brokenSprite;
+            }
+        }
+        else
+        {
+            currentDurability = durability;
+            spriteRenderer.sprite = normalSprite;
+        }
+    }
+
+    public void SaveData(GameData data)
+    {
+        data.alat[name] = currentDurability;
     }
 }
